6c1657287e5ece4f1b64e52ffe5a7aeb632a897a
blakelower <blakelower1999@gmail.com>
2020-05-09 16:14:35 -0700

    was able to add images

--- frontend/src/components/masonry-post.js
+++ frontend/src/components/masonry-post.js
@@ -2,9 +2,9 @@ import React from "react";
 import {categoryColors} from "./styles";
 
 function MasonryPost({ post, tagsOnTop }) {
-// const style = {backgroundImage: `url("${require(`../assets/images/${post.image}`)}")`};  //style={style}
+
   return (
-    <a className="masonry-post overlay"  href={post.link}> 
+    <a className="masonry-post overlay" href={post.link}> 
       <div className="image-text">
         <div className="tags-container">
           {post.categories.map((tag, ind) => 
@@ -18,6 +18,7 @@ function MasonryPost({ post, tagsOnTop }) {
         </div>
         <h2 className="image-title">{post.title}</h2>
         <span className="image-date">{post.date}</span>
+        <img src = {post.image} alt="bork"/>
       </div>
     </a>
   );
